372f287fbe0b15ffcd9d57ac1581de17eaeab377
ellie <lovejj1011@gmail.com>
2017-10-02 16:21:54 +0800

    10022

--- src/score.java
+++ src/score.java
@@ -1,2 +1,16 @@
 public class score {
+    public static void main(String[] args) {
+        //顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        double n1 = sc.nextDouble();
+        double n2 = sc.nextDouble();
+        double n3 = sc.nextDouble();
+
+        double sum = n1 + n2 + n3;
+        double average = sum / 3;
+
+        System.out.println(sum + " " + average);
+
+    }
 }
