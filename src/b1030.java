6a4e309b0b9f973c0b50e6d625cd7e2ece4b1e2f
I432 <lovejj1011@gmail.com>
2017-10-30 14:30:40 +0800

    00

---
+++ src/b1030.java
@@ -0,0 +1,17 @@
+public class b1030 {
+        public static void main(String[] args) {
+            java.util.Scanner aa = new java.util.Scanner(System.in);
+            System.out.println("請輸入成績");
+
+            int grade= aa.nextInt();
+
+            if (grade  ==100){
+                System.out.println("成績及格:" + grade+"good");
+            }
+            //else
+            {
+                //System.out.println("成績不及格:" + grade);
+            }
+        }
+    }
+
